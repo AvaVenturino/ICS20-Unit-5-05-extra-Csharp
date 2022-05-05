fe006d4fa46e8a313af0a4ba24f7041d24289f9b
Ava Venturino <99355031+AvaVenturino@users.noreply.github.com>
2022-05-05 16:34:21 +0000

    changed spacing

--- main.cs
+++ main.cs
@@ -16,19 +16,19 @@ class Program
         Console.Write("Input side 1 of triangle: ");
         sideA = Convert.ToInt32(Console.ReadLine());
         Console.Write("Input side 2 of triangle: ");
-        sideB= Convert.ToInt32(Console.ReadLine());
+        sideB = Convert.ToInt32(Console.ReadLine());
         Console.Write("Input side 3 of triangle: ");
-        sideC= Convert.ToInt32(Console.ReadLine());
-        if(sideA >0 && sideB > 0 && sideC > 0) {
-          if(sideA==sideB && sideB==sideC) {
-            Console.Write("This is an equilateral triangle.\n");
-          } else if(sideA==sideB || sideA==sideC || sideB==sideC) {
-            Console.Write("This is an isosceles triangle.\n");
+        sideC = Convert.ToInt32(Console.ReadLine());
+        if (sideA > 0 && sideB > 0 && sideC > 0) {
+          if (sideA == sideB && sideB == sideC) {
+            Console.Write ("This is an equilateral triangle.\n");
+          } else if (sideA == sideB || sideA == sideC || sideB == sideC) {
+            Console.Write ("This is an isosceles triangle.\n");
           } else {
-            Console.Write("This is a scalene triangle.\n");
+            Console.Write ("This is a scalene triangle.\n");
           }
         } else {
-          Console.Write("Input is negative. Try again.\n");
+          Console.Write ("Input is negative. Try again.\n");
         }
     }
 }

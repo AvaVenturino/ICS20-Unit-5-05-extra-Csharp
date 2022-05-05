c4ca3487ad4b11858f6b68a66cccd12bc1cc961b
Ava Venturino <99355031+AvaVenturino@users.noreply.github.com>
2022-05-05 16:02:43 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,34 @@
+// Created by: Ava Venturino
+// Created on: April 2022
+//
+// This program accepts user input
+
+using System;
+
+class Program  
+{  
+    public static void Main() 
+{  
+    int sideA;
+    int sideB;
+    int sideC; 
+    Console.Write("Check whether a triangle is Equilateral, Isosceles or Scalene:\n");
+    Console.Write("Input side 1 of triangle: ");
+    sideA= Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input side 2 of triangle: ");
+    sideB= Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input side 3 of triangle: ");
+    sideC= Convert.ToInt32(Console.ReadLine());
+    if(sideA >0 && sideB > 0 && sideC > 0) {
+      if(sideA==sideB && sideB==sideC) {  
+        Console.Write("This is an equilateral triangle.\n");  
+      } else if(sideA==sideB || sideA==sideC || sideB==sideC) {  
+        Console.Write("This is an isosceles triangle.\n");  
+      } else if (sideA!=sideB || sideA!=sideC || sideB!=sideC){  
+        Console.Write("This is a scalene triangle.\n");  
+      } else {
+        Console.Write("Input is wrong. Try again.\n");
+      }
+    }
+ }
+}

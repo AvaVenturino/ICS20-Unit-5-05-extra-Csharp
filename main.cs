3403e17fd132cfb825d4daee6fe50ef381d4b33a
Ava Venturino <99355031+AvaVenturino@users.noreply.github.com>
2022-05-05 16:51:43 +0000

    changed spaces

--- main.cs
+++ main.cs
@@ -20,7 +20,7 @@ class Program
         Console.Write("Input side 3 of triangle: ");
         sideC = Convert.ToInt32(Console.ReadLine());
         if (sideA > 0 && sideB > 0 && sideC > 0) {
-          if ( sideA == sideB && sideB == sideC ) {
+          if (sideA == sideB && sideB == sideC) {
               Console.Write ("This is an equilateral triangle.\n");
           } else if (sideA == sideB || sideA == sideC || sideB == sideC) {
               Console.Write ("This is an isosceles triangle.\n");

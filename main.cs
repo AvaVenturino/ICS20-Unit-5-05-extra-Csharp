0d89d4974689fde7114a6eb833f9db2d03f653bd
Ava Venturino <99355031+AvaVenturino@users.noreply.github.com>
2022-05-05 16:28:03 +0000

    changed spacing

--- main.cs
+++ main.cs
@@ -5,30 +5,30 @@
 
 using System;
 
-class Program 
-{  
-    public static void Main()
+class Program
 {
-    int sideA;
-    int sideB;
-    int sideC; 
-    Console.Write("Check whether a triangle is Equilateral, Isosceles or Scalene:\n");
-    Console.Write("Input side 1 of triangle: ");
-    sideA= Convert.ToInt32(Console.ReadLine());
-    Console.Write("Input side 2 of triangle: ");
-    sideB= Convert.ToInt32(Console.ReadLine());
-    Console.Write("Input side 3 of triangle: ");
-    sideC= Convert.ToInt32(Console.ReadLine());
-    if(sideA >0 && sideB > 0 && sideC > 0) {
-      if(sideA==sideB && sideB==sideC) {
-        Console.Write("This is an equilateral triangle.\n");
-      } else if(sideA==sideB || sideA==sideC || sideB==sideC) {
-        Console.Write("This is an isosceles triangle.\n");
-      } else {
-        Console.Write("This is a scalene triangle.\n");
-      }
-    } else {
-        Console.Write("Input is negative. Try again.\n");
-  }
- }
+    public static void Main(string[] args)
+    {
+        int sideA;
+        int sideB;
+        int sideC;
+        Console.Write("Check whether a triangle is Equilateral, Isosceles or Scalene:\n");
+        Console.Write("Input side 1 of triangle: ");
+        sideA= Convert.ToInt32(Console.ReadLine());
+        Console.Write("Input side 2 of triangle: ");
+        sideB= Convert.ToInt32(Console.ReadLine());
+        Console.Write("Input side 3 of triangle: ");
+        sideC= Convert.ToInt32(Console.ReadLine());
+        if(sideA >0 && sideB > 0 && sideC > 0) {
+          if(sideA==sideB && sideB==sideC) {
+            Console.Write("This is an equilateral triangle.\n");
+          } else if(sideA==sideB || sideA==sideC || sideB==sideC) {
+            Console.Write("This is an isosceles triangle.\n");
+          } else {
+            Console.Write("This is a scalene triangle.\n");
+          }
+        } else {
+          Console.Write("Input is negative. Try again.\n");
+        }
+    }
 }
